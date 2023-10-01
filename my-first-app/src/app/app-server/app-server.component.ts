4064268817aecee1e6691b3d23fb11d60d8b3686
Manish Kumar <manish.aec2012@gmail.com>
2023-10-01 21:33:35 +0530

    learning custom components

---
+++ my-first-app/src/app/app-server/app-server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-server',
+  templateUrl: './app-server.component.html',
+  styleUrls: ['./app-server.component.css']
+})
+export class AppServerComponent {
+
+}
--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -4,11 +4,17 @@ import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import {ServerComponent} from './server/server.component';
+import { AppServerComponent } from './app-server/app-server.component';
+import {WarningAlert} from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    AppServerComponent,
+    WarningAlert,
+    SuccessAlertComponent
 
   ],
   imports: [
